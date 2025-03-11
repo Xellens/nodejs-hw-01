80bb3f7fc8e3bde8e1736886020deaac8a67dd4a
Xelle <oksana2854x@gmail.com>
2025-03-11 01:03:45 +0100

    update

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...contacts, ...newContacts];
+    await writeContacts(updatedContacts);
+
+    console.log(`Додано ${number} нових контактів!`);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+      console.log('Немає контактів для видалення.');
+      return;
+    }
+
+    const removed = contacts.pop();
+    await writeContacts(contacts);
+    console.log('Видалено контакт:', removed);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 removeLastContact();
